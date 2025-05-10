7577a6531cdc78393d3d464ca40fa7deb4733929
just-fabian <fabian130404@gmail.com>
2025-05-10 06:42:44 -0400

    feat: init project

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flasgger import Swagger
+#from app.routes.preprocess_routes import preprocess_blueprint # type: ignore
+# from app.routes.detect_routes import detect_blueprint  # for future
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SWAGGER"] = {
+        "title": "Object Detection API",
+        "uiversion": 3,
+    }
+    Swagger(app)
+
+    # Register route blueprints
+    #app.register_blueprint(preprocess_blueprint, url_prefix="/preprocess")
+    # app.register_blueprint(detect_blueprint, url_prefix="/detect")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(port=5000, debug=True)
